88827b6802c53f43df1f2889f1a7becac8470749
Hisham Ismail <mhishami@gmail.com>
2017-09-23 14:54:09 +0800

    Added faq, home and press"

--- app/Http/Controllers/HomeController.js
+++ app/Http/Controllers/HomeController.js
@@ -1,12 +1,20 @@
 'use strict'
 
-// const Antl = use('Antl')
+const Antl = use('Antl')
 
 class HomeController {
 
-	* index (request, response) {
-		yield response.sendView('home')
-	}
+  * index (request, response) {
+    yield response.sendView('home')
+  }
+
+  * press (request, response) {
+    yield response.sendView('press')
+  }
+
+  * faq (request, response) {
+    yield response.sendView('faq')
+  }
 
 }
 
--- app/Http/routes.js
+++ app/Http/routes.js
@@ -19,3 +19,5 @@ const Route = use('Route')
 
 // Route.on('/').render('home')
 Route.get('/', 'HomeController.index')
+Route.get('/press', 'HomeController.press')
+Route.get('/faq', 'HomeController.faq')
