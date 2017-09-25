bd341be1e5d7643ea7ac5e3e1f7c2990486fba41
Hisham Ismail <mhishami@gmail.com>
2017-09-25 19:22:49 +0800

    Merged with repo

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 ssh staging "cd /var/www/farad; git pull"
 PID=`ssh staging ps -ef | grep node | grep -v grep | awk '{print $2}'`
 ssh staging kill -9 $PID
-ssh staging nohup make run
+ssh staging "cd /var/www/farad; nohup make run"
 
