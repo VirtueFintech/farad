a3f65e5daecf0e28b79aabf116a0669727bc2462
Hisham Ismail <mhishami@gmail.com>
2017-09-27 03:19:45 +0800

    Disable the lang

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 #
-ssh staging "cd /var/www/farad; git pull"
-PID=`ssh staging ps -ef | grep node | grep -v grep | awk '{print $2}'`
-ssh staging kill -9 $PID
-ssh staging "cd /var/www/farad; nohup make run > /dev/null 2>&1 &"
+SERVERS="farad-web1 farad-web2"
+for s in $SERVERS; do
+	ssh $s "cd /var/www/farad; git pull" 
+	PID=`ssh $s ps -ef | grep node | grep -v grep | awk '{print $2}'` 
+	ssh $s kill -9 $PID 
+	ssh $s "cd /var/www/farad; nohup make run > /dev/null 2>&1 &"
+done
 
