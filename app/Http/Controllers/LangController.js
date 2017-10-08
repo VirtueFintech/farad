703efdd460dc53ba2108f6b35b36b58dd73e4837
Rosdan Teh <rosdan@gmail.com>
2017-10-08 11:19:19 +0800

    update laguage script

--- app/Http/Controllers/LangController.js
+++ app/Http/Controllers/LangController.js
@@ -7,36 +7,44 @@ class LangController {
 
   * en (request, response) {
     Antl.setLocale('en')
+//    localStorage.setItem('en', FRLang);
     response.redirect('/')
   }
 
   * cn (request, response) {
-    Antl.setLocale('cn')
-  //  response.redirect('/')
+    Antl.setLocale('cn');
+ //   localStorage.setItem('cn', FRLang);
   response.redirect('/')
   }
 
   * kr (request, response) {
     Antl.setLocale('kr')
+  //  localStorage.setItem('id', FRLang);
+
     response.redirect('/')
   }
 
   * jp (request, response) {
     Antl.setLocale('jp')
+ //   localStorage.setItem('jp', FRLang);
+
     response.redirect('/')
   }
   
   * id (request, response) {
     Antl.setLocale('id')
+ //   localStorage.setItem('id', FRLang);
     response.redirect('/')
   }
   
   * th (request, response) {
     Antl.setLocale('th')
+  //  localStorage.setItem('th', FRLang);
     response.redirect('/')
   }
   
   * ar (request, response) {
+ //   localStorage.setItem('ar', FRLang);
     response.redirect('/ar')
   }
 
